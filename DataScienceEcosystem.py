95e7655af7815c70897f03917b99d77569f32ab4
linali14 <143889351+linali14@users.noreply.github.com>
2024-11-26 00:10:40 -0500

    Upload Data Science Notebook as Python script

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem #
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1.Python
+# 
+# 2.R
+# 
+# 3.SQL
+
+# | Data Science Tools |
+# | ------------------ |
+# | Jupyter Notebook   |
+# | RStudio            | 
+# | Apache Zeppelin    |
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1.dplyr
+# 
+# 2.ggplot
+# 
+# 3.caret
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python. ###
+
+# In[2]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[1]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+hours = 200/60
+print(hours)
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+#   
+# *List popular languages for Data Science*
+#   
+# *Code conversion hours to minutes*
+# 
+# *Multiplying and adding numbers*
+# 
+# *List popular tools for Data Science*
+# 
+# *List commonly used libreries by Data Science*
+
+# ## Author ##
+# Polina
+
+# In[ ]:
+
+
+
+
